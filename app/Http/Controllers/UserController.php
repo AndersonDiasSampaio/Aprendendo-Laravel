694b785924d17f9fe63a0a933a56992a64d41379
Anderson <anderson.dias.sampaio@gmail.com>
2020-02-12 23:23:04 -0300

    adicionando no banco de dados de forma primitiva para ver como é a sintaxe do laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function listUser(){
-
+         $user= new User();
+          $user->name= "Númeral";
+        $user->email= "aindavoucomersimonegaldino@gmail.com";
+        $user->password= Hash::Make("123");
+        $user->save();
         echo "<h1=>Listagem de Usuário</h1>";
     }
     //
