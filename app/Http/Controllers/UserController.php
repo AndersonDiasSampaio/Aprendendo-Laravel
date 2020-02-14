d2e2248cbc31c489012a67538724cbadb4aa4121
Anderson <anderson.dias.sampaio@gmail.com>
2020-02-14 01:34:07 -0300

    renomei a rota para facilitar a linkagem assim não preciso usar o link da página e sim apenas o nome da rota

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\View;
 class UserController extends Controller
 {
     public function listUser(){
-         $user= new User();
-          $user->name= "Númer1al";
-        $user->email= "aind1avoucomersimonegaldino@gmail.com";
-        $user->password= Hash::Make("123");
-        $user->save();
-        echo "<h1=>Listagem de Usuário</h1>";
+        $users= User::all();        
+        return view('user', ['users'=>$users]);
     }
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('listagem-usuario','UserController@listUser');
+Route::get('listagem-usuario','UserController@listUser')->name('listUser');
